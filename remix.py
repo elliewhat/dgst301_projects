5cd4cbcfd2617ac034a895bbfa82cb91431b9699
elliewhat <42809087+elliewhat@users.noreply.github.com>
2018-09-25 23:08:11 -0400

    Update remix.py
    added comments as to my train of thought and process for creating the code

--- remix.py
+++ remix.py
@@ -6,10 +6,11 @@ with open('HP4.txt', 'r') as file:
     text = file.read()
     
 blob = TextBlob(text)
-
+#creating the blank groups will allow my code to pull from the text file and create the content for it
 adjectives = []
 nouns = []
 determiner = []
+
 for word,pos in blob.tags:
     #print(word,pos)
     if (pos == 'JJ'):
@@ -18,10 +19,12 @@ for word,pos in blob.tags:
         nouns.append(word)
     if (pos == 'DT'):
         determiner.append(word)
-        
+ #This will be the structure of my poem, the code will randomly pull a determiner, adjective, and then noun       
 for i in range(5):
     a = random.choice(adjectives)
     n = random.choice(nouns)
     d = random.choice(determiner)
     print (d,a,n)
-#I went with 5 lines because I felt like it was a good length, not too long but not too short
+#I went with 5 lines because I felt like it was a good length, not too long but not too short.  
+#I tried to do 8-10 lines and it was just too long and wasn't making sense
+#I kept it short because every time I tried to add something else or arrange it a different way, it didn't work or sounded weird
